16e753ad334ec18972e7204b816f699a08d8b9ab
yoonstoneChoi <yuntrepreneur@gmail.com>
2021-07-15 13:54:03 +0900

    MLS-1 commit

--- perceptron/main.py
+++ perceptron/main.py
@@ -3,13 +3,44 @@ from time import time
 import pickle
 from perceptron import Perceptron
 
+## and 학습
 def step1_learning():
+    ## 학습 테스트를 위해, 사용할 데이터 
+    X = np.array([[0,0],[0,1],[1,0],[1,1]]) ## and에 대한 내용을 선언
+    y = np.array([-1,-1,-1,1]) ## 0,0 -> -1 ## 0,1 -> -1 ## 1,0 -> -1 ## 1,1 -> 1 ## and에 대한 값
+
+    ## 페셉트론 객체를 생성
     ppn = Perceptron(eta = 0.1)
-    print(ppn.eta)
+    
+    ## 학습
+    stime = time()
+    ppn.fit(X,y)
+    etime = time()
+    print('학습에 걸린 시간:', (etime - stime))
+    print('학습 중 오차가 난 갯수 :', ppn.errors_)
+
+    ## 학습이 완료된 객체를 파일로 저장한다. pickle ##with문 써서 파일 저장하는 습관이 필요함
+    with open('perceptron.pickle', 'wb') as fp:
+        pickle.dump(ppn, fp)
+    print('학습 완료, 잘 저장되었습니다.')
+
 
 def step2_using():
+    ## 파일로부터 객체를 복원
+    with open('perceptron.pickle', 'rb') as fp:
+        ppn = pickle.load(fp)
+    while True:
+        a1 = input('첫 번째 2진수를 입력 해 주세요 :')
+        a2 = input('두 번째 2진수를 입력 헤 주세요 :')
+        X = np.array([int(a1), int(a2)])
+        # 계산된 결과를 가져온다. 
+        result = ppn.predict(X)
+        if result == 1:
+            print('결과: 1, 맞음')
+        else:
+            print('-1, 틀림')
     pass
 
-step1_learning()
+#step1_learning()
 step2_using()
 
--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -13,8 +13,34 @@ class Perceptron:
 
     ## 학습 
     def fit(self, X, y):
-        pass
-        
+        ## 가중치를 담을 행렬 생성
+        ## shape[1]은 행렬 중에 행이다.
+        self.w_ = np.zeros(1 + X.shape[1]) 
+
+        #예측값과 실제값을 비교해서, 다른 예측값의 개수를 담을 리스트
+        self.errors_ = []
+        for _ in range(self.n_iter):
+            ## 예측값과 실제값이 다른 개수를 담을 변수
+            errors = 0
+
+            ## 임시변수 생성: 입력받은 입력값과 결과값을 묶는다.(pack)
+            temp1 = zip(X,y)
+
+            ## 루프돌림: 입력값과 결과값을 묵음을 가지고, 반복
+            for xi, target in temp1:
+                ## 입력값을 가지고 예측
+                a1 = self.predict(xi)
+                # 입력값과 예측값이 다른 경우 가중치 계산
+                if target != a1:
+                    update = self.eta *(target - a1)
+                    self.w_[1:] += update * xi
+                    self.w_[0] += update
+                    #값이 다른 횟수를 누적
+                    errors += int(update != 0.0)
+            ## 값이 다른 횟수값을 배열에 담는다. 
+            self.errors_.append(errors)
+            print(self.w_)
+
 
     ## 가중치(w) * 입력값(X) 총합 계산, 매트릭스 계산
     # X : 입력값      
