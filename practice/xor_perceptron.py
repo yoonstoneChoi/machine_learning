45fb1ccefafa909a064120e913879b2e41a01837
yoonstoneChoi <yuntrepreneur@gmail.com>
2021-07-18 19:56:48 +0900

    MLS-1 xor_perceptron

--- practice/xor_perceptron.py
+++ practice/xor_perceptron.py
@@ -1,18 +1,24 @@
 import numpy as np
 
 def XOR_gate_layer(x1, x2):
+    ## or 레이어
     w1_or=0.6
     w2_or=0.6
+    b_or = -0.5
+
+    ## nand 레이어
     w1_nand = -0.5
     w2_nand = -0.5
-    b_or = -0.5
     b_nand = 0.7
+
+    ## or, nand 연산
     print(x1, x2)
     result_or = x1*w1_or + x2*w2_or + b_or
     print('result_or :',result_or)
     result_nand = x1*w1_nand + x2 * w2_nand + b_nand
     print('result_nand :',result_nand)
 
+    ## xor 연산
     if result_or <= 0 or result_nand <=0:
         return 0
     else:
